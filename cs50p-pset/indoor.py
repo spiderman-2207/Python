b953c0c587eb7dcb0a41b18c66e670cbae7812b4
spiderman-2207 <289444359+spiderman-2207@users.noreply.github.com>
2026-06-14 23:34:55 +0530

    updated cs50p-pset

--- cs50p-pset/indoor.py
+++ cs50p-pset/indoor.py
@@ -1,5 +1,7 @@
-#asking input and making it in lowercase
-anystring=input("Enter the thing you wamt it in lowercase : ").lower()
+#asking input 
+anystring=input("Enter the thing you wamt it in lowercase : ")
 
+#making input into lowercase
+anystring=anystring.lower()
 #displaying userinput in lowercase 
 print(anystring)
---
+++ cs50p-pset/playback.py
@@ -0,0 +1,8 @@
+#asking user for input 
+anystring=input("Enter any string : ")
+
+#replacing space with ...
+anystring=anystring.replace(' ','...')
+
+#printing output
+print(anystring)
